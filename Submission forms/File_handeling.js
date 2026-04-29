bda764c5db060a9bc7521dde7fc6fb099df74d35
maxschultzp-cmd <maxschultzp@gmail.com>
2026-04-29 12:23:37 +0200

    Update File_handeling.js

--- Submission forms/File_handeling.js
+++ Submission forms/File_handeling.js
@@ -1,5 +1,17 @@
 const files = [];
 
+function autoGrow(el) {
+  el.style.height = '0px';
+  el.style.height = el.scrollHeight + 'px';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('textarea').forEach(ta => {
+    ta.style.height = ta.scrollHeight + 'px';
+    ta.addEventListener('input', () => autoGrow(ta));
+  });
+});
+
 function addFiles(newFiles) {
   Array.from(newFiles).forEach(f => {
     if (!files.find(x => x.name === f.name)) files.push(f);
@@ -27,3 +39,4 @@ function handleSubmit() {
   toast.classList.add('show');
   setTimeout(() => toast.classList.remove('show'), 3000);
 }
+
