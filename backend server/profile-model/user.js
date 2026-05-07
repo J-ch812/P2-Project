addb98641dc21ab548e14437d1d21579a0bd7779
sabrin <sabarin-2002@hotmail.com>
2026-05-07 13:42:26 +0200

    updated backend

--- backend server/profile-model/user.js
+++ backend server/profile-model/user.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema }   from "mongoose";
+import bcrypt from "bcrypt";
+
 const userSchema = new Schema(
     {
         username:{
@@ -36,8 +38,20 @@ const userSchema = new Schema(
     }
     
 );
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
 
-export const User= mongoose.model("User",userSchema);
+    next();
+    
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+    
+}
+
+export const User = mongoose.model("User",userSchema);
 
 
 
--- backend server/src/web.js
+++ backend server/src/web.js
@@ -1,16 +1,27 @@
 import express from "express";
-import userRouter from './router/user.router.js';
+import userRouter from '../router/user.router.js';
 
+   
 const web = express (); 
 
 
 web.use(express.json());
 
 
-web.use("/api/register", userRouter);
+web.use('/api', userRouter);
+
+  web.get("/", (req, res) => {
+   res.send("API WORKING");
 
 
-export default web;
 
+
+
+ 
+   
+
+   });
+
+   export default web;
 // http://localhost:4000/api/
 
