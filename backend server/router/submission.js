6e9a2b829794f44e658d53ea7510c0522834bae8
Julie2 <Julie2@gmail.com>
2026-05-11 19:58:32 +0100

    finished the delete user, however i change a bunch of things. not finised with css, delete profile overlay, index main, footer

--- backend server/router/submission.js
+++ backend server/router/submission.js
@@ -1,19 +1,21 @@
-import router from "./user.router.js";
-import { upload } from "./upload.js";
-router.route('/submission').post(upload.single('file'), async (req, res) => {
-    try {
+import { Router } from 'express';
+import { upload } from './upload.js';
+import Submission from '../profile-model/sub.js';
 
+const router = Router();
+
+router.post('/submission', upload.single('file'), async (req, res) => {
+    try {
         const savedSubmission = await Submission.create({
             filename: req.file.filename,
             path: req.file.path
         });
 
         res.status(200).json({
-            message: "Upload successful",
+            message: 'Upload successful',
             file: req.file,
             dbRecord: savedSubmission
         });
-
     } catch (error) {
         res.status(500).json({
             message: 'upload failed',
@@ -21,6 +23,5 @@ router.route('/submission').post(upload.single('file'), async (req, res) => {
         });
     }
 });
-
 
 export default router;
